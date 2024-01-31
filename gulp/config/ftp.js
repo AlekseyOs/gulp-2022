9cac59bc106e60fb5a144420f1d794862a80fd7a
AlekseyOs <34351228+AlekseyOs@users.noreply.github.com>
2024-02-01 01:03:15 +0300

    initial commit

---
+++ gulp/config/ftp.js
@@ -0,0 +1,15 @@
+
+// ftp data
+const ftp = {
+	host: "alekshzp.beget.tech", // Адрес FTP сервера
+	user: "alekshzp_admine", // Имя пользователя
+	password: "3&x5m%YU", // Пароль
+	parallel: 5, // Кол-во одновременных потоков
+}
+
+export let configFTP = {
+	host: ftp.host,
+	user: ftp.user, 
+	password: ftp.password,
+	parallel: ftp.parallel 
+}
---
+++ src/js/app.js
@@ -0,0 +1,10 @@
+import * as functions from "./modules/functions.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+	functions.isWebp();
+	functions.slider();
+	functions.modal();
+	functions.scrollToTop();
+	functions.fixedHeader();
+	functions.mobMenu();
+});
